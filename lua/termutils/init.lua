f61d35582a3c641aedc06b5824d24d39b075f235
dk949 <56653556+dk949@users.noreply.github.com>
2024-01-20 01:13:18 +0000

    updated docs

--- lua/termutils/init.lua
+++ lua/termutils/init.lua
@@ -1,10 +1,23 @@
 local utils = require("termutils.utils")
 local defaults = require("termutils.defaults")
+---@alias Orientation
+---| 0 Orientation.VERT
+---| 1 Orientation.HORIZ
+---| 2 Orientation.AUTO
+
 local M = {
     _opts = {},
     Orientation = defaults.Orientation
 }
 
+
+
+---Create a new terminal in a split in the direction of `orientation`
+---
+--- When using Orientation.AUTO, split is in the direcion with most space
+--- e.g. with one window on a 16:9 screen open vertical split
+---      with one window on a 9:16 screen (vertically mounted) open hirizontal split
+---@param orientation Orientation
 function M.startTerminal(orientation)
     local o = orientation or M._opts.defaultOrientation
     local w = vim.api.nvim_win_get_width(0)
@@ -18,9 +31,15 @@ function M.startTerminal(orientation)
     if M._opts.startinsert then vim.cmd [[startinsert]] end
 end
 
+--- If the current buffer was opened on the same window as a terminal, return to
+--- that terminal, otherwise close nvim
+---
+--- If the terminal no longer exists, return to the alternative buffer
+--- If no alternative buffer exists, return to the last created buffer
+--- If no more buffers are available, close nvim.
 function M.smartClose()
     ---@diagnostic disable-next-line: param-type-mismatch
-    assert(M._opts.smartClose, "Cannot call termutils.smartClose if smartClose option is set to false")
+    assert(M._opts.smartClose, "Cannot call termutils.smartClose if smartClose option is set to false in setup")
     local winnr = vim.fn.winnr()
 
     if vim.bo.buftype ~= "terminal" and vim.tbl_contains(_G.termutils.termWindow, winnr) then
@@ -43,7 +62,7 @@ function M.smartClose()
             for buf = vim.fn.bufnr('$'), 0, -1 do
                 if buf ~= this_bufnr then return buf end
             end
-            -- the nvr buffer we're about to close is the last buffer there is
+            -- the nvr buffer we're about to close is the last buffer there is, so just close nvim
             return -1
         end)()
         if buf ~= -1 then
